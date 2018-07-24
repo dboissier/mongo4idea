f529a5872e5120a14fa2ccbf71494dedbaabeeb7
David Boissier <david.boissier@gmail.com>
2018-07-24 09:03:41 +0200

    update Apache License header

--- src/main/java/org/codinjutsu/tools/mongo/view/console/MongoConsoleRunner.java
+++ src/main/java/org/codinjutsu/tools/mongo/view/console/MongoConsoleRunner.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2016 David Boissier.
+ * Copyright (c) 2018 David Boissier.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
